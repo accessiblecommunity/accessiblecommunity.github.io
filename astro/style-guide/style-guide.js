41d7296494c7bc1776a733c7735b1f1fc4725a8a
Brian Montgomery <brian@accessiblecommunity.org>
2024-01-09 12:12:55 -0500

    First wave of refactoring themes and colors

--- astro/style-guide/style-guide.js
+++ astro/style-guide/style-guide.js
@@ -32,8 +32,8 @@ export default {
     </button>
   </li>
   <li class="nav-item" role="presentation">
-    <button class="nav-link" id="style-guide-colors" data-bs-toggle="tab" data-bs-target="#style-guide-colors-pane" type="button" role="tab" aria-controls="style-guide-colors-pane" aria-selected="false">
-      Colors
+    <button class="nav-link" id="style-guide-themes" data-bs-toggle="tab" data-bs-target="#style-guide-themes-pane" type="button" role="tab" aria-controls="style-guide-themes-pane" aria-selected="false">
+      Themes
     </button>
   </li>
   <li class="ms-auto nav-item me-1 my-1" role="presentation">
@@ -126,49 +126,28 @@ export default {
     </div>
   </div>
 
-  <div class="tab-pane fade" id="style-guide-colors-pane" role="tabpanel" aria-labelledby="style-guide-colors" tabindex="0">
-    <div class="row">
-      <div class="col col-md-6">
-        <div class="d-flex justify-content-center gap-2">
-          <p>Backgrounds:</p>
-          <div>
-            <div class="rounded border border-ac">&nbsp; &nbsp;</div>
-            <span>(default)</span>
-          </div>
-          <div>
-            <div class="rounded border bg-ac">&nbsp; &nbsp;</div>
-            <span><code>ac</code> (Our brand)</span>
-          </div>
-          <div>
-            <div class="rounded border border-ac bg-body-secondary">&nbsp; &nbsp;</div>
-            <code>body-secondary</code>
-          </div>
-          <div>
-            <div class="rounded border border-ac bg-body-tertiary">&nbsp; &nbsp;</div>
-            <code>body-tertiary</code>
-          </div>
-        </div>
+  <div class="tab-pane fade" id="style-guide-themes-pane" role="tabpanel" aria-labelledby="style-guide-themes" tabindex="0">
+    <div class="d-flex justify-content-center align-items-center gap-2">
+      <div class="px-3 py-2 text-body-emphasis bg-body border border-3 rounded">
+        Body
       </div>
-      <div class="col col-md-6">
-        <div class="d-flex justify-content-center gap-2">
-          <p>Foregrounds:</p>
-          <div>
-            <div class="rounded border" style="background-color: var(--bs-body-color);">&nbsp; &nbsp;</div>
-            <span>(default)</span>
-          </div>
-          <div>
-            <div class="rounded border bg-ac">&nbsp; &nbsp;</div>
-            <span><code>ac</code> (Our brand)</span>
-          </div>
-          <div>
-            <div class="rounded border border-ac" style="background-color: var(--bs-secondary-color);">&nbsp; &nbsp;</div>
-            <code>secondary</code>
-          </div>
-          <div>
-            <div class="rounded border border-ac" style="background-color: var(--bs-tertiary-color);">&nbsp; &nbsp;</div>
-            <code>tertiary</code>
-          </div>
-        </div>
+      <div class="px-3 py-2 text-bg-primary border border-primary border-3 rounded">
+        Primary
+      </div>
+      <div class="px-3 py-2 text-primary-emphasis bg-primary-subtle border border-primary-subtle border-3 rounded">
+        Primary (Subtle)
+      </div>
+      <div class="px-3 py-2 text-bg-secondary border border-secondary border-3 rounded">
+        Secondary
+      </div>
+      <div class="px-3 py-2 text-secondary-emphasis bg-secondary-subtle border border-secondary-subtle border-3 rounded">
+        Secondary Subtle
+      </div>
+      <div class="px-3 py-2 bg-body-tertiary text-body-tertiary border border-tertiary border-3 rounded">
+        Tertiary
+      </div>
+      <div class="px-3 py-2 text-tertiary-emphasis bg-tertiary-subtle border border-tertiary-subtle border-3 rounded">
+        Tertiary Subtle
       </div>
     </div>
   </div>
