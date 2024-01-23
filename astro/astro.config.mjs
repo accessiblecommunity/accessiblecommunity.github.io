446ed671c8c9e28e9aa2207cc198bf2db8d418e1
alexisreytech <alexis.rey.tech@gmail.com>
2024-01-23 16:06:37 -0600

    Convert Markdown files to MDX and use the External Link component for external links within blog posts.

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,28 +1,30 @@
 import { defineConfig } from 'astro/config';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import styleGuide from './style-guide/register.js'
+import styleGuide from './style-guide/register.js';
 
 // https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
+import mdx from "@astrojs/mdx";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.accessiblecommunity.org',
-  server: { host: true },
+  server: {
+    host: true
+  },
   vite: {
     resolve: {
       alias: {
         '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        '~bootstrap-es': path.resolve(__dirname, 'node_modules/bootstrap/dist/js/bootstrap.esm.min.js'),
+        '~bootstrap-es': path.resolve(__dirname, 'node_modules/bootstrap/dist/js/bootstrap.esm.min.js')
       }
     },
     ssr: {
-      noExternal: ['bootstrap'],
-    },
+      noExternal: ['bootstrap']
+    }
   },
-  integrations: [
-    styleGuide(),
-  ]
+  integrations: [styleGuide(), mdx()]
 });
