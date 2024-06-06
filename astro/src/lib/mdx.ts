20b1a03877a29d456fdc950156b41072b607b46e
Brian Montgomery <brian@accessiblecommunity.org>
2024-06-06 13:56:43 -0400

    Use branding for emphasis instead of strong in mdx

--- astro/src/lib/mdx.ts
+++ astro/src/lib/mdx.ts
@@ -3,4 +3,4 @@ import Link from '../components/mdx/Link.astro';
 import ImageFigure from "../components/ImageFigure.astro";
 export { ImageFigure };
 
-export const components = { a: Link, strong: Branding };
+export const components = { a: Link, em: Branding };
