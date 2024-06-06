e27712b7f5f1976ee064969a17bba0262b0451af
Brian Montgomery <brian@accessiblecommunity.org>
2024-06-05 21:32:47 -0400

    Incorporate branding and mdx changes

---
+++ astro/src/lib/mdx.ts
@@ -0,0 +1,6 @@
+import Branding from '../components/Branding.astro';
+import Link from '../components/mdx/Link.astro';
+import ImageFigure from "../components/ImageFigure.astro";
+export { ImageFigure };
+
+export const components = { a: Link, strong: Branding };
