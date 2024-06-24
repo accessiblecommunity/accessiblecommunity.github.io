0d96665fbaeb055b148c66fc65f36b03b3d38cec
brian-montgomery <brian-montgomery@users.noreply.github.com>
2024-06-24 18:38:11 -0400

    Blog/Tip Enhancements (#297)
    * Issue #285: Refactor MDX custom components into layouts/components.
    * Issue #296: Use the Container API to add the content to the blog feeds.

--- astro/src/lib/mdx.ts
+++ astro/src/lib/mdx.ts
@@ -1,6 +1,18 @@
+/** @module mdx */
+
 import Branding from "../components/Branding.astro";
 import Link from "../components/mdx/Link.astro";
-import ImageFigure from "../components/ImageFigure.astro";
-export { ImageFigure };
 
+// Options for sanitize-html, to keep RSS clean
+export const sanitizeOptions = {
+    allowedTags: [
+        'p', 'code', 'span', 'b', 'strong', 'p', 'img', 'figure', 'figcaption', 'ul', 'ol', 'li',
+    ],
+    // TODO: Determine way of injecting BS rules into CSS XML
+    // allowedAttributes: {
+    //     "span": [ "class", ],
+    // }
+}
+// MDX Components to translate default Markdown syntax.
 export const components = { a: Link, em: Branding };
+export default components;
