161ba1a073fc7cb3a3627380d5af55ded8790b6d
Brian Montgomery <brian@accessiblecommunity.org>
2024-06-24 10:14:22 -0400

    Clean up MDX imports

--- astro/src/lib/mdx.ts
+++ astro/src/lib/mdx.ts
@@ -1,6 +1,8 @@
+/** @module mdx */
+
 import Branding from "../components/Branding.astro";
 import Link from "../components/mdx/Link.astro";
-import ImageFigure from "../components/ImageFigure.astro";
-export { ImageFigure };
 
+// MDX Components to translate default Markdown syntax.
 export const components = { a: Link, em: Branding };
+export default components;
