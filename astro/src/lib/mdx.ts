a23900ae98dbbfd44523b2151a71cde4a51aeb44
Brian Montgomery <brian@accessiblecommunity.org>
2024-08-01 21:26:51 -0400

    Updates to Blog

--- astro/src/lib/mdx.ts
+++ astro/src/lib/mdx.ts
@@ -1,18 +1,31 @@
 /** @module mdx */
 
 import Branding from "../components/Branding.astro";
+import Blockquote from "../components/mdx/Blockquote.astro";
 import Link from "../components/mdx/Link.astro";
 
 // Options for sanitize-html, to keep RSS clean
 export const sanitizeOptions = {
-    allowedTags: [
-        'p', 'code', 'span', 'b', 'strong', 'p', 'img', 'figure', 'figcaption', 'ul', 'ol', 'li',
-    ],
-    // TODO: Determine way of injecting BS rules into CSS XML
-    // allowedAttributes: {
-    //     "span": [ "class", ],
-    // }
-}
+  allowedTags: [
+    "p",
+    "code",
+    "span",
+    "b",
+    "strong",
+    "p",
+    "img",
+    "figure",
+    "figcaption",
+    "ul",
+    "ol",
+    "li",
+    "blockquote",
+  ],
+  // TODO: Determine way of injecting BS rules into CSS XML
+  // allowedAttributes: {
+  //     "span": [ "class", ],
+  // }
+};
 // MDX Components to translate default Markdown syntax.
-export const components = { a: Link, em: Branding };
+export const components = { a: Link, em: Branding, blockquote: Blockquote };
 export default components;
