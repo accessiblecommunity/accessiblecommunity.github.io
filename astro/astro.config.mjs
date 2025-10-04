bda8903cf0c6b36e6a2e40f1e980d3116b8ab672
Ashim Gautam Upadhaya <asimupdgtm@gmail.com>
2025-10-04 10:52:56 -0500

    Issue #411: "About our escape rooms" section (#410)
    ---------
    
    Co-authored-by: Ashim Gautam Upadhaya <ashimupadhaya@Ashims-Air.hitronhub.home>

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -69,7 +69,7 @@ export default defineConfig({
           // Social Media
           'facebook', 'instagram', 'linkedin', 'rss-fill', 'tiktok', 'youtube','globe', 'mastodon', 'twitter',
           // Descriptive
-          'gift-fill', 'pencil-fill', 'people-fill', 'person-fill', 'stopwatch-fill', 'tools',
+          'gift-fill', 'pencil-fill', 'people-fill', 'person-fill', 'puzzle-fill', 'stopwatch-fill', 'tools',
         ],
         // CoreUI Brands
         cib: [
